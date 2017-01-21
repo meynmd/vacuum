2e26e641dd5a414d63e7a6999919de984a7efd6d
DURGAHARISH DAYAPULE <dimbul@10-248-137-26.wireless.oregonstate.edu>
2017-01-21 13:17:45 -0800

    new testing code

--- agentModel.py
+++ agentModel.py
@@ -45,6 +45,13 @@ class ModelCleaner(cleaner.Cleaner):
 			# if not isFacingWall and self.moveForward == 1 and self.NextAction==1:
 			# 	self.moveForward=0
 
+			if isFacingWall and self.moveForward:
+				if self.IsSweepingNorth:
+					self.moveForward=0
+					return self.ActTurnLeft
+				else:
+					self.moveForward=0
+					return self.ActTurnLeft
 
 
 			if isFacingWall and self.IsSweepingNorth:
